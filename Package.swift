57cde91f3191460ec4757d99b3a0c46004d11b6f
curtistandy <curtistandy0754@gmail.com>
2024-09-15 12:19:19 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/shoumo680/XrayKit/releases/download/0.1.45/XrayKit.xcframework.zip",
+      url: "https://github.com/curtistandy/XrayKit/releases/download/0.1.45/XrayKit.xcframework.zip",
       checksum: "541f9bf3f1b79f5c9f88899eab5f47c021ef7f0aa52c4373d9cb9cb236407afc"
     )
   ]
